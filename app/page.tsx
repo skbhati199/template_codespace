83b3e234b8227cf5a48e7291355c0e3504dd3c71
Sonu Kumar <skbhati199@gmail.com>
2023-04-23 14:56:36 +0000

    initital codespace init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-3xl'>Hello World</h1>
+    </div>
+  )
+}
